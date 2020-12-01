4653aeeeb560642d97221e4380abc3db88c0ba01
Isaac Santos <isaacdsantos@hotmail.com>
2020-12-01 18:26:03 -0300

    modificado index e main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ function clicou(){
 function redirecionar(){
 	window.open('https://www.google.com.br/');
 }
-function trocar(){
-	document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse";
+function trocar(elemento){
+	elemento.innerHTML="Obrigado por passar o mouse";
 }
-function voltar(){
-	document.getElementById("mousemove").innerHTML="Passe o mouse aqui.";
+function voltar(elemento){
+	elemento.innerHTML="Passe o mouse aqui.";
 }
