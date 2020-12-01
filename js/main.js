7ef728ac3115f372013346f68b2f82528b9f27bd
Isaac Santos <isaacdsantos@hotmail.com>
2020-12-01 18:15:57 -0300

    adicionado nova funcionalidade ao main.js e modificado index

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function clicou(){
 	document.getElementById("agradecimento").innerHTML="Obrigado por clicar";
 }
+function redirecionar(){
+	window.open('https://www.google.com.br/');
+}
+function trocar(){
+	document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse";
+}
